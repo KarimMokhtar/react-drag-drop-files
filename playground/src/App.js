204fe5dbcdf5d874523ea53044c85751aa41d992
Karim Mokhtar <karimmokhtar28@gmail.com>
2021-09-09 17:53:43 +0200

    feat: add playground for dev and testing

---
+++ playground/src/App.js
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import { FileUploader } from "react-drag-drop-files";
+import "./App.css";
+const fileTypes = ["JPG", "PNG", "GIF", "JPEG"];
+
+function App() {
+  const [file, setFile] = useState(null);
+  const handleChange = file => {
+    setFile(file);
+    console.log("changesss", file);
+  };
+  const onDrop = file => {
+    console.log("drop", file);
+  };
+  const onSelect = file => {
+    console.log("test", file);
+  };
+
+  const onTypeError = (err = 1) => console.log(err);
+  const onSizeError = (err = 1) => console.log(err);
+  console.log(file);
+  return (
+    <div className="App">
+      <FileUploader
+        classes="testttt"
+        file={file}
+        onTypeError={onTypeError}
+        handleChange={handleChange}
+        name="image"
+        types={fileTypes}
+        onSizeError={onSizeError}
+        onDrop={onDrop}
+        onSelect={onSelect}
+      />
+      <br />
+      <button onClick={() => setFile(null)}>Clear File</button>
+    </div>
+  );
+}
+
+export default App;
