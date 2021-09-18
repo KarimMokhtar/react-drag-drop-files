e3bea2621795ad50e13532950914606ae2b711a8
Karim Mokhtar <karimmokhtar28@gmail.com>
2021-09-18 06:32:45 +0200

    chore: adding first test for checking draw types

--- tests/DrawTypes.test.tsx
+++ tests/DrawTypes.test.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import React from "react";
 import { render, screen } from "@testing-library/react";
+import DrawTypes from "../src/DrawTypes";
 
 describe("drawing types of the required file", () => {
   test("Rendering nothing when there are no types provided", () => {
-    // render(<DrawTypes />);
-    // const linkElement = screen.getByText(/test/i);
-    // expect(linkElement).toBeInTheDocument();
-    expect(5).toBe(5);
+    const { container } = render(<DrawTypes />);
+    expect(container.childElementCount).toEqual(0);
+   
   });
+
 });
