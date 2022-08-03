8949a2ef0def7ef40a8d4ee15506b6430ed500d1
Karim Mokhtar <karimmokhtar28@gmail.com>
2022-08-03 02:04:44 +0200

    fix: revert back to void

--- src/useDragging.tsx
+++ src/useDragging.tsx
@@ -5,7 +5,7 @@ type Params = {
   labelRef: any;
   inputRef: any;
   multiple?: boolean | false;
-  handleChanges: (arg0: Array<File>) => boolean;
+  handleChanges: (arg0: Array<File>) => void;
   onDrop?: (arg0: Array<File>) => void;
 };
 
