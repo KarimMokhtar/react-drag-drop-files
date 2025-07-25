82dbd998fab7802651a085737f7c92ce8fc7e0d7
Matt Cohen <matt.cohen@veeva.com>
2025-07-25 10:50:49 -0700

    file drag in detection for safari

--- src/useDragging.tsx
+++ src/useDragging.tsx
@@ -32,7 +32,7 @@ export default function useDragging({
     ev.preventDefault();
     ev.stopPropagation();
     draggingCount++;
-    if (ev.dataTransfer.items && ev.dataTransfer.items.length !== 0) {
+    if (ev.dataTransfer.items && ev.dataTransfer.items.length !== 0 || ev.dataTransfer.types?.indexOf('Files') >= 0) {
       setDragging(true);
     }
   }, []);
